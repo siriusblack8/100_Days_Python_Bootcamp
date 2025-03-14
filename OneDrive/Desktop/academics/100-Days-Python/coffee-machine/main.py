366123aa7537a53ecf88bf1629d0c8040ed0b1b6
Bhavya Krishnan <131344877+siriusblack8@users.noreply.github.com>
2025-03-14 20:38:38 +0530

    main.py

--- OneDrive/Desktop/academics/100-Days-Python/coffee-machine/main.py
+++ OneDrive/Desktop/academics/100-Days-Python/coffee-machine/main.py
@@ -1,4 +1,5 @@
 def making_coffee(total_resource,order_resources,money):
+    """changes the resources available after processing the order"""
     total_resource["Water"]-=order_resources["Water"]
     total_resource["Coffee"]-=order_resources["Coffee"]
     total_resource["Milk"]-=order_resources["Milk"]
@@ -8,42 +9,34 @@ def making_coffee(total_resource,order_resources,money):
     print("\n")
     return (total_resource)
     
-def report(a):
+def report(a,b):
+    """give a report on resources available"""
     print(f"Water: {a["Water"]}ml")
-    print("\n")
     print(f"Coffee: {a["Coffee"]}g")
-    print("\n")
     print(f"Milk: {a["Milk"]}ml")
-    print("\n")
     print(f"Money: ${a["Money"]}")
+    print(f"Profit: ${a["Money"]- b}")
     print("\n")
-    print("\n")
-    print(f"Profit: ${a["Money"]-100}")
     
 def Money_calculation():
+    """calculates the total amount input by the user"""
     print("Please input money in coins")
     print("\n")
     amount_paid = 0
     value_coins = {"quarters":0.25 , "dimes":0.10 , "nickles":0.05 , "pennies":0.01}
     for coins in value_coins:
         count = int(input(f"Enter how many {coins}: "))
-        print("\n")
         amount_paid += count*value_coins[coins]
+    print("\n")
     return(amount_paid)
     
 def resource_sufficiency(total_resource,order_resources,money):
-    if total_resource["Water"]<order_resources["Water"]:
-        print("Sorry, there is not enough water! Money refunded")
-        print("\n")
-        return False
-    if total_resource["Coffee"]<order_resources["Coffee"]:
-        print("Sorry, there is not enough coffee! Amount refunded")
-        print("\n")
-        return False
-    if total_resource["Milk"]<order_resources["Milk"]:
-        print("Sorry, there is not enough milk! Money refunded")
-        print("\n")
-        return False
+    """checks if the resources are sufficient for the order placed"""
+    for items in total_resource:
+        if total_resource[items]<order_resources[items]:
+            print(f"Sorry, there is not enough {items}! Money refunded")
+            print("\n")
+            return False
     if money<order_resources["Money"]:
         print("Amount not enough! Money refunded ")
         print("\n")
@@ -52,6 +45,7 @@ def resource_sufficiency(total_resource,order_resources,money):
     
 def coffee_machine():
     resources = {"Milk": 1000 , "Coffee": 500 , "Water": 2500, "Money": 100 }
+    initial_amount = resources["Money"]
     cont = 1
     while cont == 1:
         customer_input = (input("What would you like to order? (americano/latte/cappuccino):")).lower()
@@ -67,7 +61,7 @@ def coffee_machine():
             return
         elif customer_input == "report":
             print("\n")
-            report(resources)
+            report(resources, initial_amount)
         else:
             amount_paid = Money_calculation()
             if resource_sufficiency(resources, resources_required[customer_input], amount_paid) == True:
